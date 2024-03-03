80037394ffd9d9d83fbc6b05440e7dba1f20ea6d
Mike Pleasant <mike.pmail@gmail.com>
2024-03-03 10:42:11 -0500

    Added entity and start of arch

--- company_rework_2024_top.vhdl
+++ company_rework_2024_top.vhdl
@@ -6,17 +6,36 @@ use ieee.numeric_std.all;
 --Entity declaration
 entity company_rework_2024_top is
     port(
-        clk                : in std_logic;
-        reset              : in std_logic;
-        in_ready           : out std_logic;
-        in_valid           : in std_logic;
-        in_start_of_packet : in std_logic;
-        in_end_of_packet   : in std_logic;
-        in_data            : in std_logic_vector(63 downto 0);
-        in_empty           : in std_logic_vector(2 downto 0);
-        in_error           : in std_logic;
+        CLK                : in std_logic;
+        RESET              : in std_logic;
+        
+        IN_VALID           : in std_logic;
+        IN_START_OF_PACKET : in std_logic;
+        IN_END_OF_PACKET   : in std_logic;
+        IN_DATA            : in std_logic_vector(63 downto 0);
+        IN_EMPTY           : in std_logic_vector(2 downto 0);
+        IN_ERROR           : in std_logic;
+        
+        IN_READY           : out std_logic;
         OUT_VALID          : out std_logic;
         OUT_DATA           : out std_logic_vector(255 downto 0);
-        out_byte_mask      : out std_logic_vector(31 downto 0)
+        OUT_BYTE_MASK      : out std_logic_vector(31 downto 0)
     );
 end company_rework_2024_top;
+
+--Architecture declaration
+architecture rtl of company_rework_2024_top is
+    --Signal declaration
+    signal state         : std_logic_vector(2 downto 0);
+    signal data          : std_logic_vector(255 downto 0);
+    signal byte_mask     : std_logic_vector(31 downto 0);
+    signal ready         : std_logic;
+    signal out_valid     : std_logic;
+    signal out_data      : std_logic_vector(255 downto 0);
+    signal out_byte_mask : std_logic_vector(31 downto 0);
+    
+    --Process declaration
+    begin
+
+end architecture rtl;
+```
