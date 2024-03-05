d2d4f4fa6d1249441010b20040ff9c9e5d4bce21
Mike Pleasant <mike.pmail@gmail.com>
2024-03-04 21:35:21 -0500

    Added ports to files

---
+++ data_shifter_reg.vhdl
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity data_shift_mux is
+    port (
+        CLK           : in  std_logic;
+        RESET_N       : in  std_logic;
+        
+        DATA_IN       : in  std_logic_vector(63 downto 0);
+        DATA_IN_RDY   : in  std_logic;
+        DATA_IN_WEN_N : in  std_logic_vector(7 downto 0);
+
+        DATA_OUT      : out std_logic_vector(255 downto 0)
+    );
+end entity data_shift_mux;
+
+architecture rtl of data_shift_mux is
+    
+begin
+
+end architecture rtl;
---
+++ message_receiver.vhdl
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity message_receiver is
+    port (
+        CLK                : in  std_logic;
+        RESET_N            : in  std_logic;
+        
+        IN_VALID           : in  std_logic;
+        IN_START_OF_PACKET : in  std_logic;
+        IN_END_OF_PACKET   : in  std_logic;
+        IN_DATA            : in  std_logic_vector(63 downto 0);
+        IN_EMPTY           : in  std_logic;
+        IN_ERROR           : in  std_logic;
+   
+        IN_READY           : OUT  std_logic;
+        OUT_VALID          : OUT  std_logic;
+        OUT_BYTE_MASK      : OUT  std_logic_vector(31 downto 0);
+
+        OUT_BYTES          : OUT  std_logic_vector(63 downto 0);
+        OUT_BYTES_EN_N     : OUT  std_logic_vector(7 downto 0);
+        OUT_BYTES_RDY      : OUT  std_logic;
+
+        LAST_BYTE_CNT      : OUT  std_logic_vector(2 downto 0)
+    );
+end entity message_receiver;
+
+architecture rtl of message_receiver is
+    
+begin
+
+end architecture rtl;
