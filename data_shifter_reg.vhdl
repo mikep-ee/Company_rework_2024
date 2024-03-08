4702e379966109f2331223e6a751f42588b226cc
Mike Pleasant <mike.pmail@gmail.com>
2024-03-07 22:29:53 -0500

    Add MAX_BYTES and LAST_BYTE_CNT to interface

--- data_shifter_reg.vhdl
+++ data_shifter_reg.vhdl
@@ -7,9 +7,12 @@ entity data_shift_reg is
         CLK           : in  std_logic;
         RESET_N       : in  std_logic;
         
-        DATA_IN       : in  std_logic_vector(63 downto 0);
-        DATA_IN_RDY   : in  std_logic;
+        DATA_IN       : in  std_logic_vector(63 downto 0); -- 8bytes of data to be shifted in
+        DATA_IN_RDY   : in  std_logic;                     
         DATA_IN_WEN_N : in  std_logic_vector(7 downto 0);
+        
+        MAX_BYTES     : in  std_logic_vector(2 downto 0);
+        LAST_BYTE_CNT : in  std_logic_vector(2 downto 0);
 
         DATA_OUT      : out std_logic_vector(255 downto 0)
     );
